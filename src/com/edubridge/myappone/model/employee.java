1ce5d94c8a6cf1463add415ef132b6ff16ece3e8
MALKA SRINIVAS <MALKA SRINIVAS@DESKTOP-6MEJ894>
2024-09-24 13:11:28 +0530

    getter and setter added

--- src/com/edubridge/myappone/model/employee.java
+++ src/com/edubridge/myappone/model/employee.java
@@ -2,6 +2,24 @@ package com.edubridge.myappone.model;
 
 public class employee {
 		private int employeeId;
+		public int getEmployeeId() {
+			return employeeId;
+		}
+		public void setEmployeeId(int employeeId) {
+			this.employeeId = employeeId;
+		}
+		public String getEmployeeName() {
+			return employeeName;
+		}
+		public void setEmployeeName(String employeeName) {
+			this.employeeName = employeeName;
+		}
+		public long getEmployeeSalary() {
+			return employeeSalary;
+		}
+		public void setEmployeeSalary(long employeeSalary) {
+			this.employeeSalary = employeeSalary;
+		}
 		private String employeeName;
 		private long employeeSalary;
 		
